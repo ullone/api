78c22d30279ef98305a1ad947bf6ac2cd14318a7
ullone <ullone@163.com>
2017-09-25 17:23:21 +0800

    fix

---
+++ application/index/controller/Index.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\index\controller;
+
+use \think\View;
+
+class Index {
+    public function index() {
+        $this->checkToken();
+        // $view = new \think\View();
+        // return $view->fetch();
+    }
+                                   fa
+    private function checkToken() {
+        $signature = $_GET['signature'];
+        $timestamp = $_GET['timestamp'];
+        $rand      = $_GET['rand'];
+        $token     = '3bfe75bfb2d4752e';
+        $tmpArr = array($token, $timestamp, $rand);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        if($tmpStr == $signature) {
+          echo sha1($token);
+          exit;
+        }
+    }
+}
