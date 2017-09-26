afa19be911c277c6e78fd9db41989e99ed21c7a8
ullone <ullone@163.com>
2017-09-26 14:34:15 +0800

    fix

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -14,7 +14,7 @@ class Upload{
     //构造函数
     public function __construct()
     {
-        move_uploaded_file('/php','/webdata/api/upload');die;
+        // move_uploaded_file('/php','/webdata/api/upload');die
         $this->upload_name = $_FILES["file"]["name"]; //取得上传文件名
         $this->upload_filetype = $_FILES["file"]["type"];
         $this->upload_tmp_name = $_FILES["file"]["tmp_name"];
@@ -38,6 +38,7 @@ class Upload{
                 }
                 $this->upload_final_name = date("YmdHis").rand(0,100).'.'.$upload_filetype;//生成随机文件名
                 $this->upload_target_path = $this->upload_target_dir."/".$this->upload_final_name;//文件上传目标目录
+                eixt(move_uploaded_file($this->upload_tmp_name,$this->upload_target_path));
                 if(!move_uploaded_file($this->upload_tmp_name,$this->upload_target_path))//文件移动失败
                 {
                     exit('success');
