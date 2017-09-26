a82721e32e3b58178ef82b7fe452a6ad0e88d8aa
ullone <ullone@163.com>
2017-09-26 09:48:47 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -27,7 +27,7 @@ class Index {
     }
 
     private function saveFile() {
-      $result = $_FILES["file"];
+      $result = $_FILES["file"]["name"];
       var_dump($result);var_dump('test');die;
       // $myfile = fopen("newfile.txt", "a+") or die("Unable to open file!");
       // $txt = "Bill Gates\n";
