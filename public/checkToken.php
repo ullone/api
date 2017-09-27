8b3e4e60c61865f33e011a3006a79c9128b12fe3
ullone <ullone@163.com>
2017-09-27 15:32:48 +0800

    fix

---
+++ public/checkToken.php
@@ -0,0 +1,13 @@
+<?php
+
+//服务器校验
+$token = sha1("8b0b255ffb10cd97");
+echo $token;
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $token.'\n');
+$txt = "Steve Jobs\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+exit();
+
+?>
--- public/index.php
+++ public/index.php
@@ -11,16 +11,6 @@
 
 // [ 应用入口文件 ]
 
-//服务器校验
-$token = sha1("8b0b255ffb10cd97");
-echo $token;
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $token.'\n');
-$txt = "Steve Jobs\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-exit();
-
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
