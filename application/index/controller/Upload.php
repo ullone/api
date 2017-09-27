3e8fc43b470d8171f56abf3aa7410a112a12e445
ullone <ullone@163.com>
2017-09-27 15:59:27 +0800

    fix

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -76,13 +76,13 @@ class Upload{
     *@param String $filePath要获取文件的绝对路径
     */
     private function silkToWav($filePath, $fileName) {
+      var_dump($fileName);
       $file = $filePath.'/'.$fileName;
       $cmd  = '/webdata/api/upload/silk-v3-decoder-master/converter.sh '.$file.' wav';
       exec($cmd,$output);
       //转码成功
       $pos     = strripos($fileName, '.'); //获取到文件名的位置
       $name    = substr($file, 0, $pos); //获取文件名
-            var_dump($name);die;
       $wavFile = $filePath.'/'.$name.'wav';
       $this->voiceToText($wavFile);
     }
