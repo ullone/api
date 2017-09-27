8f4fb14886daf590806ca9e7afd4e2cc68e464b9
ullone <ullone@163.com>
2017-09-27 17:41:09 +0800

    fix

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -129,7 +129,7 @@ class Upload{
     	curl_close($ch);
       $data = json_decode($response, true);
       // $data = json_encode($data, JSON_UNESCAPED_UNICODE);
-    	var_dump($data);die;
+    	exit($data);
     }
 }
 ?>
