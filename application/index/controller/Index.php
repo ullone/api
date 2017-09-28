551b6ffc94dfe81044d809792db3d8c41e0efe90
ullone <ullone@163.com>
2017-09-28 16:25:01 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,7 +10,7 @@ class Index {
         // $view = new \think\View();
         // return $view->fetch();\
         $upload = new Upload();
-        $upload->voiceToText('a', 'b');
+        $upload->silkToWav('a', 'b');
     }
 
     private function checkToken() {
