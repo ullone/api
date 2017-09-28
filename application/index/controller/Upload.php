659614f646c88451d478a72277915fde19041975
ullone <ullone@163.com>
2017-09-28 15:06:57 +0800

    fix

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -91,7 +91,7 @@ class Upload{
       // $content   = hex2bin($content);
       // echo $content;die;
       $tmp       = base64_encode($content);
-      $text      = 'data='.$tmp;
+      $text      = $tmp;
       $timestamp = time();
       $param     = array('auf' => '8k', 'aue' => 'raw', 'scene' => 'main');
       $param     = base64_encode(json_encode($param));
