54b64d6b0cc42b4df01b36151efef01f0c749e02
ullone <ullone@163.com>
2017-09-28 11:42:54 +0800

    fix

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -88,7 +88,8 @@ class Upload{
       // header( "Content-type: txt");
       $handle    = fopen($file,"rb");
       $content   = fread($handle,filesize($file));
-      echo($content);die;
+      $content   = hex2bin($content);
+      echo $content;die;
       $tmp       = base64_encode($content);
       $text      = 'data='.$tmp;
       $timestamp = time();
