316739b6b64878f2b703a075e9e54aadddab42ce
ullone <ullone@163.com>
2017-09-29 16:51:29 +0800

    fix

--- application/index/controller/Aio.php
+++ application/index/controller/Aio.php
@@ -21,25 +21,40 @@ class Aio {
       $text      = base64_encode($text);
       $text      = 'text='.$text;
       $timestamp = time();
-      //生成param参数
-      $userid = '';
-      for ($i = 0; $i < 10; $i++)
-      {
-        $userid .= chr(mt_rand(33, 126));
-      }
-      $param    = array('scene' => 'main', 'userid' => $userid);
-      $param    = base64_encode(json_encode($param));
-      $checkSum = '2465d5f2471a4bf297ed6e4e144a18f3'.$timestamp.$param.$text;
-      $checkSum = md5($checkSum);
-      $url      = 'http://api.xfyun.cn/v1/aiui/v1/text_semantic';
-      $data     = array(
+      $param     = array('auf' => '16k', 'aue' => 'raw', 'scene' => 'main');
+      $param     = base64_encode(json_encode($param));
+      $checkSum  = md5('156607c2a7704572ab0fbaa632a04880'.$timestamp.$param.$text);
+      $url       = 'http://api.xfyun.cn/v1/aiui/v1/text_semantic';
+      $data      = array(
         'timestamp' => $timestamp,
         'checkSum'  => $checkSum,
         'param'     => $param,
         'text'      => $text
       );
-      $data = Func::doCurl($url, 'post', $data);
-      $data = json_decode($data, true);
+      $this->doCurl($url, 'post', $data);
+    }
+
+    private function doCurl($url, $method = 'get', $data = null) {
+      $header = [
+        "X-Appid:59c37565",
+        "X-CurTime:".$data['timestamp'],
+        "X-Param:".$data['param'],
+        "X-CheckSum:".$data['checkSum'],
+      ];
+    	$ch = curl_init();
+    	curl_setopt($ch, CURLOPT_URL, $url);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    	if($method == 'post') {
+    		curl_setopt($ch, CURLOPT_POST, 1);
+    		curl_setopt($ch, CURLOPT_POSTFIELDS, $data['text']);
+    	}
+    	$response = curl_exec($ch);
+    	if(curl_errno($ch)){
+    		print curl_error($ch);
+    	}
+    	curl_close($ch);
+      $data = json_decode($response, true);
       $data = json_encode($data, JSON_UNESCAPED_UNICODE);
       exit($data);
     }
