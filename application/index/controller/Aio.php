98234936a8f87e490ca24405fb9bb0331bda93a9
ullone <ullone@163.com>
2017-09-29 14:24:12 +0800

    fix

---
+++ application/index/controller/Aio.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\index\controller;
+
+use app\index\controller\Func;
+
+class Aio {
+  private $text;
+  public function __construct() {
+    $this->text = isset($_POST['text'])?$_POST['text'] : null;
+  }
+
+  public function index() {
+    if($this->text == null) exit('要转义的内容不能为空');
+    $this->semanticComprehension();
+  }
+
+  public function semanticComprehension() {
+      $text      = $this->text;
+      $text      = base64_encode($text);
+      $text      = 'text='.$text;
+      $timestamp = time();
+      //生成param参数
+      $userid = '';
+      for ($i = 0; $i < 10; $i++)
+      {
+        $userid .= chr(mt_rand(33, 126));
+      }
+      $param    = array('scene' => 'main', 'userid' => $userid);
+      $param    = base64_encode(json_encode($param));
+      $checkSum = 'daa3e49549c8481389ef01d2a4488f88'.$timestamp.$param.$text;
+      $checkSum = md5($checkSum);
+      $url      = 'http://api.xfyun.cn/v1/aiui/v1/text_semantic';
+      $data     = array(
+        'timestamp' => $timestamp,
+        'checkSum'  => $checkSum,
+        'param'     => $param,
+        'text'      => $text
+      );
+      Func::doCurl($url, 'post', $data);
+    }
+
+}
---
+++ application/index/controller/Func.php
@@ -0,0 +1,87 @@
+<?php
+namespace app\index\controller;
+
+class Func {
+  public static function callBack($code, $msg, $data = null, $isExist = true) {
+    $data = Func::revert($data);
+    $result = json_encode([
+        'code'   =>  $code,
+        'msg'     =>  $msg,
+        'data'     =>  $data,
+      ], JSON_UNESCAPED_UNICODE);
+    if(!$isExist) return $result;
+    exit($result);
+  }
+
+  public static function getToken() {
+    foreach($_SERVER as $key => $value) {
+      if($key === 'HTTP_TOKEN')
+        return $value;
+    }
+  }
+
+  private static function revert($data) {
+    if(!is_array($data))
+      return $data;
+    $newData = array();
+    foreach ($data as $key => $value) {
+      $key = ($key === strtoupper($key))?strtolower($key):$key;
+      $key = str_replace(' ', '', ucwords(str_replace('_', ' ', $key)));
+      $key[0] = strtolower($key[0]);
+      if(is_array($value))
+        $value = Func::revert($value);
+      $newData[$key] = $value;
+    }
+    return $newData;
+  }
+
+  public static function randStr($length, $type = 'all') {
+    if ($type == 'all')
+      $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    elseif ($type == 'num')
+      $chars='1234567890';
+    $str = "";
+    for ($i = 0; $i < $length; $i++)
+      $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+    return $str;
+  }
+
+  public static function getTime($format = null) {
+    $format = empty($format)?'Y-m-d H:i:s':$format;
+    return date($format);
+  }
+
+  public static function debug($data = null) {
+    exit(var_dump($data));
+  }
+
+  public static function js2Array($data = null) {
+    if(is_string($data)) return json_decode($data, true);
+    else return $data;
+  }
+
+  public static function doCurl($url, $method = 'get', $data = null) {
+    $header = [
+      "X-Appid:59c37565",
+      "X-CurTime:".$data['timestamp'],
+      "X-Param:".$data['param'],
+      "X-CheckSum:".$data['checkSum'],
+    ];
+  	$ch = curl_init();
+  	curl_setopt($ch, CURLOPT_URL, $url);
+  	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+  	if($method == 'post') {
+  		curl_setopt($ch, CURLOPT_POST, 1);
+  		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  	}
+  	$response = curl_exec($ch);
+  	if(curl_errno($ch)){
+  		print curl_error($ch);
+  	}
+  	curl_close($ch);
+    $data = json_decode($response, true);
+    $data = json_encode($data, JSON_UNESCAPED_UNICODE);
+    exit($data);
+  }
+}
