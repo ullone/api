a487bf2f9fea26ab65b19924eeadf4cf693b5189
ullone <ullone@163.com>
2017-09-29 17:08:59 +0800

    fix

--- application/index/controller/Comprehension.php
+++ application/index/controller/Comprehension.php
@@ -37,8 +37,30 @@ class Comprehension {
         'param'     => $param,
         'text'      => $text
       );
-      $data = Func::doCurl($url, 'post', $data);
-      $data = json_decode($data, true);
+      $this->doCurl($url, 'post', $data);
+    }
+
+    private function doCurl($url, $method = 'get', $data = null) {
+      $header = [
+        "X-Appid:59c37565",
+        "X-CurTime:".$data['timestamp'],
+        "X-Param:".$data['param'],
+        "X-CheckSum:".$data['checkSum'],
+      ];
+    	$ch = curl_init();
+    	curl_setopt($ch, CURLOPT_URL, $url);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    	if($method == 'post') {
+    		curl_setopt($ch, CURLOPT_POST, 1);
+    		curl_setopt($ch, CURLOPT_POSTFIELDS, $data['text']);
+    	}
+    	$response = curl_exec($ch);
+    	if(curl_errno($ch)){
+    		print curl_error($ch);
+    	}
+    	curl_close($ch);
+      $data = json_decode($response, true);
       $data = json_encode($data, JSON_UNESCAPED_UNICODE);
       exit($data);
     }
