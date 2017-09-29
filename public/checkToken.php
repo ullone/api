e335851ca34a866d8f069c87cf50ae9f6e6ec8d3
ullone <ullone@163.com>
2017-09-29 16:43:22 +0800

    fix

--- public/checkToken.php
+++ public/checkToken.php
@@ -1,13 +1,13 @@
 <?php
 
 //服务器校验
-$token = sha1("8b0b255ffb10cd97");
+$token = sha1("c6261995f22ab4c7");
 echo $token;
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $token.'\n');
-$txt = "Steve Jobs\n";
-fwrite($myfile, $txt);
-fclose($myfile);
+// $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+// fwrite($myfile, $token.'\n');
+// $txt = "Steve Jobs\n";
+// fwrite($myfile, $txt);
+// fclose($myfile);
 exit();
 
 ?>
