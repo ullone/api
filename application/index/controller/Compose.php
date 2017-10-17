c67c1c14b6dc81282bfac2687a58db5aa5116c29
ullone <ullone@163.com>
2017-10-17 17:31:37 +0800

    fix

--- application/index/controller/Compose.php
+++ application/index/controller/Compose.php
@@ -9,62 +9,11 @@ class Compose {
   public function voiceCompose() {
     header("Content-type: audio/mp3");
     $text = urlencode('好的，已经为您记录');
-    $access_token = $this->getAccessToken('1jzCUFD9pjaysq4TLULYs1Qk','aBQTEe3Pf8YtZaeok5T8nDaAX60CyxOz');
+    $access_token = Func::getAccessToken('1jzCUFD9pjaysq4TLULYs1Qk','aBQTEe3Pf8YtZaeok5T8nDaAX60CyxOz');
     $token        = $access_token;
     $cuid         = uniqid();
     $url          = "http://tsn.baidu.com/text2audio?tex=$text&lan=zh&cuid=$cuid&ctp=1&tok=$access_token";
     $res    = file_get_contents($url);
     var_dump($res);die;
-  }
-
-  /**
-   * 获取API访问授权码
-   * @param ak: ak from baidu cloud app
-   * @param sk: sk from baidu cloud app
-   * @return - access_token string if succeeds, else false.
-  */
-  private function getAccessToken($ak, $sk) {
-    $url = 'https://aip.baidubce.com/oauth/2.0/token';
-    $post_data = array();
-    $post_data['grant_type']  = 'client_credentials';
-    $post_data['client_id']   = $ak;
-    $post_data['client_secret'] = $sk;
-    $res = $this->requestPost($url, $post_data);
-    if (!!$res) {
-        $res = json_decode($res, true);
-        return $res['access_token'];
-    } else {
-        return false;
-    }
-  }
-
-  /**
-   * 发起http post请求(REST API), 并获取REST请求的结果
-   * @param string $url
-   * @param string $param
-   * @return - http response body if succeeds, else false.
-  */
-  private function requestPost($url = '', $param = '') {
-    if (empty($url) || empty($param)) {
-        return false;
-    }
-
-    $postUrl = $url;
-    $curlPost = $param;
-    // 初始化curl
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $postUrl);
-    curl_setopt($curl, CURLOPT_HEADER, 0);
-    // 要求结果为字符串且输出到屏幕上
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    // post提交方式
-    curl_setopt($curl, CURLOPT_POST, 1);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $curlPost);
-    // 运行curl
-    $data = curl_exec($curl);
-    curl_close($curl);
-
-    return $data;
   }
 }
--- application/index/controller/Func.php
+++ application/index/controller/Func.php
@@ -83,4 +83,55 @@ class Func {
   	curl_close($ch);
     return $response;
   }
+
+  /**
+   * 获取API访问授权码
+   * @param ak: ak from baidu cloud app
+   * @param sk: sk from baidu cloud app
+   * @return - access_token string if succeeds, else false.
+  */
+  public static function getAccessToken($ak, $sk) {
+    $url = 'https://aip.baidubce.com/oauth/2.0/token';
+    $post_data = array();
+    $post_data['grant_type']  = 'client_credentials';
+    $post_data['client_id']   = $ak;
+    $post_data['client_secret'] = $sk;
+    $res = Func::requestPost($url, $post_data);
+    if (!!$res) {
+        $res = json_decode($res, true);
+        return $res['access_token'];
+    } else {
+        return false;
+    }
+  }
+
+  /**
+   * 发起http post请求(REST API), 并获取REST请求的结果
+   * @param string $url
+   * @param string $param
+   * @return - http response body if succeeds, else false.
+  */
+  public static function requestPost($url = '', $param = '') {
+    if (empty($url) || empty($param)) {
+        return false;
+    }
+
+    $postUrl = $url;
+    $curlPost = $param;
+    // 初始化curl
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $postUrl);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    // 要求结果为字符串且输出到屏幕上
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    // post提交方式
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $curlPost);
+    // 运行curl
+    $data = curl_exec($curl);
+    curl_close($curl);
+
+    return $data;
+  }
 }
