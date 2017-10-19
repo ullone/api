62a4c539a18572e6a7efc74b3d3fdb33e50a6857
ullone <ullone@163.com>
2017-10-19 18:12:24 +0800

    fix

--- application/index/service/UserSer.php
+++ application/index/service/UserSer.php
@@ -7,7 +7,7 @@ use app\index\service\Func;
 
 class UserSer {
   public static function login($openid) {
-    if(empty($openid)) Func::callBack('登陆失败');
+    if(empty($openid)) Func::callBack(301,'登陆失败');
     if(!($uid = User::find(array('openid' => $openid), 'id'))) {
       $uid = User::addOne(array('openid' => $openid, 'create_time' => date('Y-m-d H:i:s')));
     }
