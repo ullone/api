7e0a6598b14cf14edb999753037c1a43a84853e7
ullone <ullone@163.com>
2017-10-20 18:17:25 +0800

    fix

--- application/index/controller/UserCon.php
+++ application/index/controller/UserCon.php
@@ -5,6 +5,7 @@ namespace app\index\controller;
 use \think\Cookie;
 use \think\Cache;
 use app\index\service\UserSer;
+use app\index\service\ComposeSer;
 
 class UserCon {
 
@@ -16,5 +17,10 @@ class UserCon {
   public function index() {
     $code = isset($_POST['code']) ? $_POST['code'] : null;
     UserSer::login($code);
+  }
+
+  public function test() {
+    $file = ComposeSer::voiceCompose('很抱歉，这句话我不理解');
+    var_dump($file);die;
   }
 }
