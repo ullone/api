33ea855fb03b12ff5a53b0f45c4ab8cd3f9e98f9
ullone <ullone@163.com>
2017-10-20 12:05:56 +0800

    fix

--- application/index/service/UserSer.php
+++ application/index/service/UserSer.php
@@ -32,31 +32,32 @@ class UserSer {
 
 public static function getUrandom($min = 0, $max = 0x7FFFFFFF)
 {
-        $diff = $max - $min;
-        if ($diff > PHP_INT_MAX) {
-            throw new RuntimeException('Bad Range');
-        }
-
-        $fh = fopen('/dev/urandom', 'r');
-        stream_set_read_buffer($fh, PHP_INT_SIZE);
-        $bytes = fread($fh, PHP_INT_SIZE );
-        if ($bytes === false || strlen($bytes) != PHP_INT_SIZE ) {
-            //throw new RuntimeException("nable to get". PHP_INT_SIZE . "bytes");
-            return 0;
-        }
-        fclose($fh);
-
-        if (PHP_INT_SIZE == 8) { // 64-bit versions
-            list($higher, $lower) = array_values(unpack('N2', $bytes));
-            $value = $higher << 32 | $lower;
-        }
-        else { // 32-bit versions
-            list($value) = array_values(unpack('Nint', $bytes));
-
-        }
-
-        $val = $value & PHP_INT_MAX;
-        $fp = (float)$val / PHP_INT_MAX; // convert to [0,1]
-
-        return (int)(round($fp * $diff) + $min);
+  return 1;
+        // $diff = $max - $min;
+        // if ($diff > PHP_INT_MAX) {
+        //     throw new RuntimeException('Bad Range');
+        // }
+        //
+        // $fh = fopen('/dev/urandom', 'r');
+        // stream_set_read_buffer($fh, PHP_INT_SIZE);
+        // $bytes = fread($fh, PHP_INT_SIZE );
+        // if ($bytes === false || strlen($bytes) != PHP_INT_SIZE ) {
+        //     //throw new RuntimeException("nable to get". PHP_INT_SIZE . "bytes");
+        //     return 0;
+        // }
+        // fclose($fh);
+        //
+        // if (PHP_INT_SIZE == 8) { // 64-bit versions
+        //     list($higher, $lower) = array_values(unpack('N2', $bytes));
+        //     $value = $higher << 32 | $lower;
+        // }
+        // else { // 32-bit versions
+        //     list($value) = array_values(unpack('Nint', $bytes));
+        //
+        // }
+        //
+        // $val = $value & PHP_INT_MAX;
+        // $fp = (float)$val / PHP_INT_MAX; // convert to [0,1]
+        //
+        // return (int)(round($fp * $diff) + $min);
 }
