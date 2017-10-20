ad8200b0304f33a39cbdd902aa1619e3200bd68c
ullone <ullone@163.com>
2017-10-20 09:44:28 +0800

    fix

--- application/index/controller/UserCon.php
+++ application/index/controller/UserCon.php
@@ -7,7 +7,7 @@ use app\index\service\UserSer;
 
 class UserCon {
   public function index() {
-    $openid = 'test';//isset($_POST['openid']) ? $_POST['openid'] : null;
+    $openid = isset($_POST['openid']) ? $_POST['openid'] : null;
     UserSer::login($openid);
   }
 }
