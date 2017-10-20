f8ba9a9af3b73150af6fcbd17b713af0caf36877
ullone <ullone@163.com>
2017-10-20 11:18:55 +0800

    fix

--- application/index/controller/UserCon.php
+++ application/index/controller/UserCon.php
@@ -8,7 +8,7 @@ use app\index\service\UserSer;
 class UserCon {
   public function index() {
     $code = isset($_POST['code']) ? $_POST['code'] : null;
-    var_dump($code);die ;
+    var_dump($_POST);die ;
     UserSer::login($code);
   }
 }
