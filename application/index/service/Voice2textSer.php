96487f02cab74e89be6e559c288ea7f83bb8e00e
ullone <ullone@163.com>
2017-10-23 17:11:10 +0800

    fix

--- application/index/service/Voice2textSer.php
+++ application/index/service/Voice2textSer.php
@@ -30,7 +30,7 @@ class Voice2textSer {
       Func::callBack(107, '调用讯飞语音接口失败，请检查参数');
     if(empty($data['data']['result'])) {
       $file = ComposeSer::voiceCompose('很抱歉，这句话我难以理解!,请重新录制');
-      Func::callBack(108, '语音识别失败', $file);
+      Func::callBack('108', '语音识别失败', $file);
     }
     return $data['data']['result'];
   }
