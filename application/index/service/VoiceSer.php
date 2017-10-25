8b1a51452288c8f6aa350804171587d1969a55d4
ullone <ullone@163.com>
2017-10-25 12:01:02 +0800

    fix

--- application/index/service/VoiceSer.php
+++ application/index/service/VoiceSer.php
@@ -10,6 +10,7 @@ class VoiceSer {
   public static function getData($uid) {
     $start = date('Y-m-d');
     $end   = date('Y-m-d',strtotime('+1 week'));
-    Voice::select($uid, $start, $end);
+    $data  = Voice::select($uid, $start, $end);
+    Func::callBack(0, '获取成功', $data);
   }
 }
