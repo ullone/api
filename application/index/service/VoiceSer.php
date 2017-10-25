c0b54bdaf9a1ec713eae30e9b97750d5d9f934ae
ullone <ullone@163.com>
2017-10-25 15:08:27 +0800

    fix

--- application/index/service/VoiceSer.php
+++ application/index/service/VoiceSer.php
@@ -10,8 +10,10 @@ class VoiceSer {
   public static function getData($uid) {
     $start = date('Y-m-d');
     $end   = date('Y-m-d',strtotime('+1 week'));
-    // var_dump($uid);die;// var_dump($start);var_dump($end);die;
     $data  = Voice::select($uid, $start, $end);
+    foreach($data as $key) {
+      $key['oclock'] = substr($key['oclock'], 5);
+    }
     Func::callBack(0, '获取成功', $data);
   }
 }
